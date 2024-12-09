001458477caa10c2b6bbd0b3c2b495a74d85f42d
vanyadarkov <phonedarkov@gmail.com>
2024-12-09 21:54:29 +0200

    In api route create newsletter subscription with service method

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,9 +1,22 @@
 import { NextResponse } from "next/server";
 import { newsletterSchema } from "@/const/newsletter";
+import { createNewsletterSubscription } from "@/app/service/newsletterService";
 
 export async function POST(request: Request) {
   try {
-    const body = await request.json();
+    let body;
+    try {
+      body = await request.json();
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json(
+        {
+          message: "Could not get data",
+          success: false,
+        },
+        { status: 400 },
+      );
+    }
 
     const validatedFields = newsletterSchema.safeParse({
       firstName: body.firstName,
@@ -26,50 +39,14 @@ export async function POST(request: Request) {
 
     const { firstName, lastName, message, email } = validatedFields.data;
 
-    try {
-      const response = await fetch("https://formspree.io/f/xkgnaolk", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          firstName,
-          lastName,
-          email,
-          message,
-        }),
-      });
-
-      if (!response.ok) {
-        const errorResponse = await response.json();
-        return NextResponse.json(
-          {
-            message:
-              "Failed to subscribe to the newsletter. Please try again later.",
-            success: false,
-            errors: errorResponse.errors || null,
-          },
-          { status: 400 },
-        );
-      }
-
-      return NextResponse.json(
-        {
-          message: "Thank you for subscribing to our newsletter!",
-          success: true,
-        },
-        { status: 200 },
-      );
-    } catch (error) {
-      console.error("Error while subscribing to the newsletter:", error);
-      return NextResponse.json(
-        {
-          message: "An unexpected error occurred. Please try again later.",
-          success: false,
-        },
-        { status: 400 },
-      );
-    }
+    await createNewsletterSubscription(firstName, lastName, email, message);
+    return NextResponse.json(
+      {
+        message: "Thank you for subscribing to our newsletter!",
+        success: true,
+      },
+      { status: 200 },
+    );
   } catch (error) {
     console.log(error);
     return NextResponse.json(

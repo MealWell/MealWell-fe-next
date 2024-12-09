297ab815d4b12fafc38d92a06d619cffebf5b45b
vanyadarkov <phonedarkov@gmail.com>
2024-12-09 21:32:19 +0200

    Disabling posthog on dev env

--- app/posthog-provider.js
+++ app/posthog-provider.js
@@ -2,12 +2,24 @@
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
-if (typeof window !== "undefined") {
+const shouldInitializePostHog =
+  typeof window !== "undefined" && process.env.NODE_ENV !== "development";
+
+if (shouldInitializePostHog) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
+} else {
+  // Provide a noop implementation of PostHog to avoid errors in development
+  posthog.capture = () => {};
+  posthog.identify = () => {};
 }
+
 export function CSPostHogProvider({ children }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  return (
+    <PostHogProvider client={shouldInitializePostHog ? posthog : null}>
+      {children}
+    </PostHogProvider>
+  );
 }

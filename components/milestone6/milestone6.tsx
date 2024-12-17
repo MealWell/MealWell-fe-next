d60247a9b1fe7f94538f87780000bcbf194958d3
Denis Rusu <denis@globalsoftsys.ro>
2024-12-17 23:40:26 +0200

    Milestone 6. Target market size

---
+++ components/milestone6/milestone6.tsx
@@ -0,0 +1,11 @@
+import TargetMarketSize from "./targetMarketSize";
+
+const Milestone6 = () => {
+  return (
+    <div className="space-y-6">
+      <TargetMarketSize />
+    </div>
+  );
+};
+
+export default Milestone6;

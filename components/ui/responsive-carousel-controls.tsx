b918c31e32389831d1b3cbb29f0241aca8e3d76f
vanyadarkov <phonedarkov@gmail.com>
2025-01-07 07:11:35 +0200

    Fixed issue with carousel controls inside a form that would submit it

--- components/ui/responsive-carousel-controls.tsx
+++ components/ui/responsive-carousel-controls.tsx
@@ -1,28 +1,32 @@
-import {CarouselNext, CarouselPrevious} from "@/components/ui/carousel";
-import {cn} from "@/lib/utils";
+import { CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
 
 export default function ResponsiveCarouselControls() {
-    return (<>
-        <div className="hidden lg:block">
-            <CarouselPrevious/>
-        </div>
-        <div className="hidden lg:block">
-            <CarouselNext/>
-        </div>
-        <div className="flex gap-8 mt-4 lg:hidden">
-            <CarouselPrevious
-                className={cn(
-                    "static h-8 w-8 rounded-full",
-                    "-left-0 top-0 -translate-y-0",
-                )}
-            />
-            <div className="flex-grow"/>
-            <CarouselNext
-                className={cn(
-                    "static h-8 w-8 rounded-full",
-                    "-right-0 top-0 -translate-y-0",
-                )}
-            />
-        </div>
-    </>);
+  return (
+    <>
+      <div className="hidden lg:block">
+        <CarouselPrevious type={"button"} />
+      </div>
+      <div className="hidden lg:block">
+        <CarouselNext type={"button"} />
+      </div>
+      <div className="flex gap-8 mt-4 lg:hidden">
+        <CarouselPrevious
+          type={"button"}
+          className={cn(
+            "static h-8 w-8 rounded-full",
+            "-left-0 top-0 -translate-y-0",
+          )}
+        />
+        <div className="flex-grow" />
+        <CarouselNext
+          type={"button"}
+          className={cn(
+            "static h-8 w-8 rounded-full",
+            "-right-0 top-0 -translate-y-0",
+          )}
+        />
+      </div>
+    </>
+  );
 }
